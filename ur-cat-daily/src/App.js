f4940db38acfcc6004a1b5b6159dda9b792c5e0d
ceciPino <mepino@uc.cl>
2022-08-30 17:40:37 -0400

    creating react app

---
+++ ur-cat-daily/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p> ladies welcome to ur dosis diaria of kittens </p>
+    </div>
+  );
+}
+
+export default App;
