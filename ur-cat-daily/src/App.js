088ac626f04984c5f3fffe36b6ea8b088eed321a
ceciPino <mepino@uc.cl>
2022-09-01 17:50:28 -0400

    showin da pics

--- ur-cat-daily/src/App.js
+++ ur-cat-daily/src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [catImage, setCatImage] = useState(null);
+
+
+  useEffect(() => {
+    fetch("https://api.thecatapi.com/v1/images/search", {headers: {
+      'x-api-key': 'live_Had3ALgPydzhJW2vYswAjmgyvXLGYz7VrGBtonkJ5nqOKIt7xVTysss2W9uFx3zW'
+    }})
+      .then((response) => response.json())
+      .then((cat) => {
+        setCatImage(cat);
+        setIsLoading(false);
+      });
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="App">
+        <h1>Cargando...</h1>
+      </div>
+    );
+  }
+
+  console.log(catImage);
+
+
+  console.log(catImage[0].url);
+
   return (
     <div className="App">
-      <p> ladies welcome to ur dosis diaria of kittens </p>
+      <p> Hello ladies welcome to ur dosis diaria of kittens </p>
+      <img src={catImage[0].url} alt="Imagen de kitty aleatoria"/>
     </div>
   );
 }
